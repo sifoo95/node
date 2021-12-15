6c945d4d2fb7a96ef73c091374280ad732d305ce
sefi <seyfeddineghrimil8@gmail.com>
2021-12-15 22:36:43 +0100

    first commit

---
+++ filtered-ls.js
@@ -0,0 +1,6 @@
+const fs=require('fs')
+const path = require('path')
+
+ fs.readdir(process.argv[2], (err,list)=>{
+    err? `erreur `: list.forEach (el=> (path.extname(el) == `.${process.argv[3]}`)? console.log(el): '');
+})
